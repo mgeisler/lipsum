b4b2df2ab81211b3f6e4fa76b8553b730235c0d0
Martin Geisler <martin@geisler.net>
2017-06-13 01:21:46 +0200

    MarkovChain: add simple Markov chain implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+extern crate rand;
+
+use std::collections::HashMap;
+use rand::Rng;
+
+/// A bigram is simply two consecutive words.
+type Bigram<'a> = (&'a str, &'a str);
+
+#[derive(Default)]
+pub struct MarkovChain<'a> {
+    map: HashMap<Bigram<'a>, Vec<&'a str>>,
+}
+
+// Based on
+// https://blakewilliams.me/posts/generating-arbitrary-text-with-markov-chains-in-rust
+impl<'a> MarkovChain<'a> {
+    /// Create a new Markov chain. It will use a default thread-local
+    /// random number generator.
+    pub fn new() -> MarkovChain<'a> {
+        MarkovChain { map: HashMap::new() }
+    }
+
+    /// Add new text to the Markov chain. This can be called several
+    /// times to build up the chain.
+    pub fn learn(&mut self, sentence: &'a str) {
+        let words = sentence.split_whitespace().collect::<Vec<&str>>();
+        for window in words.windows(3) {
+            let (a, b, c) = (window[0], window[1], window[2]);
+            self.map.entry((a, b)).or_insert(vec![]).push(c);
+        }
+    }
+
+    /// Generate `n` words worth of lorem ipsum text. The text will
+    /// start from a random point in the Markov chain.
+    pub fn generate(&self, n: usize) -> String {
+        if self.map.is_empty() {
+            // The learn method has not been called.
+            return String::new();
+        }
+
+        let mut rng = rand::thread_rng();
+        let keys = self.map.keys().collect::<Vec<&Bigram>>();
+        let (mut a, mut b) = **rng.choose(&keys).unwrap();
+        let mut sentence = String::from(a) + " " + b;
+
+        for _ in 0..n {
+            while !self.map.contains_key(&(a, b)) {
+                let new_key = **rng.choose(&keys).unwrap();
+                a = new_key.0;
+                b = new_key.1;
+            }
+
+            let next_words = &self.map[&(a, b)];
+            let c = rng.choose(next_words).unwrap();
+            sentence.push(' ');
+            sentence.push_str(c);
+            a = b;
+            b = c;
+        }
+
+        return sentence;
+    }
+}
+
 /// The traditional lorem ipsum text as given in [Wikipedia]. Using
 /// this text alone for a Markov chain of order two doesn't work very
 /// well since each bigram (two consequtive words) is followed by just
@@ -30,5 +94,22 @@ mod tests {
     #[test]
     fn starts_with_lorem_ipsum() {
         assert_eq!(&lipsum()[..11], "Lorem ipsum");
+    }
+
+    #[test]
+    fn empty_chain() {
+        let chain = MarkovChain::new();
+        assert_eq!(chain.generate(10), "");
+    }
+
+    #[test]
+    fn chain_map() {
+        let mut chain = MarkovChain::new();
+        chain.learn("foo bar baz quuz");
+        let map = &chain.map;
+
+        assert_eq!(map.len(), 2);
+        assert_eq!(map[&("foo", "bar")], vec!["baz"]);
+        assert_eq!(map[&("bar", "baz")], vec!["quuz"]);
     }
 }
