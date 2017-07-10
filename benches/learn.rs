355b9703e654b8852a25c3ed79aac7b3355e26e1
Martin Geisler <martin@geisler.net>
2017-07-10 17:25:17 +0200

    rustfmt: add config file
    This configuration mostly matches the existing code style, which I
    happen to like. Compared to the new style agreed upon in the RFC
    process, this style uses more visual layouts.
    
    With the struct_lit_width setting, small struct literals are put onto
    a single line.

--- benches/learn.rs
+++ benches/learn.rs
@@ -9,15 +9,15 @@ use test::Bencher;
 #[bench]
 fn learn_lorem_ipsum(b: &mut Bencher) {
     b.iter(|| {
-        let mut chain = lipsum::MarkovChain::new();
-        chain.learn(lipsum::LOREM_IPSUM)
-    })
+               let mut chain = lipsum::MarkovChain::new();
+               chain.learn(lipsum::LOREM_IPSUM)
+           })
 }
 
 #[bench]
 fn learn_liber_primus(b: &mut Bencher) {
     b.iter(|| {
-        let mut chain = lipsum::MarkovChain::new();
-        chain.learn(lipsum::LIBER_PRIMUS)
-    })
+               let mut chain = lipsum::MarkovChain::new();
+               chain.learn(lipsum::LIBER_PRIMUS)
+           })
 }
