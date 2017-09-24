ad9ba72b2ad759466c9cf9ce44918191e9c80245
Martin Geisler <martin@geisler.net>
2017-09-24 14:04:49 +0200

    lib: set html_root_url
    This is per C-HTML-ROOT in the Rust API Guidelines:
    https://rust-lang-nursery.github.io/api-guidelines/documentation.html

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@
 //! [`learn`]: struct.MarkovChain.html#method.learn
 //! [Markov chain]: https://en.wikipedia.org/wiki/Markov_chain
 
+#![doc(html_root_url = "https://docs.rs/lipsum/0.3.0")]
 #![deny(missing_docs)]
 
 extern crate rand;
